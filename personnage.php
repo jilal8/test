ae27d99124380d98722e50d5eb7caeaf63f1cff1
jilal8 <34911762+jilal8@users.noreply.github.com>
2017-12-28 17:36:34 +0100

    mise a jour de la class personnage
    ajout de la documentation de la class personnage

--- personnage.php
+++ personnage.php
@@ -1,29 +1,57 @@
 <?php 
+	/**
+	 * 
+	 */
 	class Personnage{
 
-		private $nombreVie = 5;
-		private $nomPersonnage ;
+		public $nombreVie = 5;
+		public $nomPersonnage ;
+		/**
+		 * @param int $nombreVie le nombre de vie du personnage
+		 * @param string $nom le nom du personnage
+		 */
 
-		public function __construct($nom){
+		public function __construct($nombreVie, $nom){
+			$this->nombreVie = $nombreVie;
 			$this->nomPersonnage = $nom;
 		}
+		/**
+		*
+		*cette methode permet de nous retourner le nombre de vie de notre personnage
+		*@param String $nom  le nom de notre personnage
+		**/
 
 		public function setNom($nom){
 			$this->nomPersonnage = $nom;
 		}
 
+		/**
+		*
+		*cette methode permet de nous mettre a jour le nombre de vie de notre personnage
+		*@param $nbre le nombre de vie 
+		*
+		**/
 		public function setNombrevie($nbre){
 			$this->nombreVie = $nbre;
 		}
-
+		
+		/**
+		*
+		*cette methode permet de nous retourner le nom de notre personnage
+		*@return String nom du personnage
+		**/
 		public function getNom(){
 			return $this->nomPersonnage;
 		}
-
+		
+		/**
+		*
+		*cette methode permet de nous retourner le nombre de vie de notre personnage
+		*@return int nombre de vie
+		**/
 		public function getNombrevie(){
 			return $this->nombreVie;
 		}
 
 	}
-
  ?>

5435910cd8e2e94d56c2948d668d8679c0c741f4
jilal8 <34911762+jilal8@users.noreply.github.com>
2017-12-28 12:31:55 +0100

    formulaire

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
 <?php 
-	include 'Personnage.php';
-
-	$matie = new Personnage(2,"mahdi");
-
-	$matie->setNom("jilal_mahdi_hassanMHD");
-
-	var_dump($matie->getNom());
-
+	$form = new Form();
+	$form->input("username");
+	$form->input("username");
+	$form->input("username");
  ?>

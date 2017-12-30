22f61ad7865d639be6b1081c74af47438a44c808
jilal8 <34911762+jilal8@users.noreply.github.com>
2017-12-30 19:52:03 +0100

    mise a jour 1

--- traitementIncsr.php
+++ traitementIncsr.php
@@ -5,10 +5,10 @@ require 'index.php';
 	
 		if(!empty($_POST)){
 			var_dump($_POST);
-			 /*$nom=$_POST['nom'];
+			 $nom=$_POST['nom'];
 			 $email = $_POST['mail'];
 			 $pass=$_POST['password'];
-			 $pass1=$_POST['password1'];*/
+			 $pass1=$_POST['password1'];
 
 			 
 

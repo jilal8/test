2320d74a926247ccbc28067a500337e1a6e0bf71
jilal8 <34911762+jilal8@users.noreply.github.com>
2017-12-30 19:51:17 +0100

    fichier permettant de les données poster

---
+++ traitementIncsr.php
@@ -0,0 +1,27 @@
+<?php 
+require 'index.php';
+
+
+	
+		if(!empty($_POST)){
+			var_dump($_POST);
+			 /*$nom=$_POST['nom'];
+			 $email = $_POST['mail'];
+			 $pass=$_POST['password'];
+			 $pass1=$_POST['password1'];*/
+
+			 
+
+		}else{
+			echo "nulll";
+			die();
+		}
+		
+
+		
+
+
+	 
+
+
+ ?>

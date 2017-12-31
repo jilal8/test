44448f4db107b844db5079935f6f6be6f02e926a
jilal8 <34911762+jilal8@users.noreply.github.com>
2017-12-31 17:58:30 +0100

    fichier de javascipt

---
+++ testJs.php
@@ -0,0 +1,7 @@
+  var button = document.getElementById("lieninscription");
+    var divinscription = document.getElementById("inscription");
+        button.onclick = function(){
+            divinscription.style.visibility ="visible";
+            button.style.visibility = "hidden";
+            alert("je suis fatigué merd marche.");
+        };
